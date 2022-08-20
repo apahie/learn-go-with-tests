5ce5a576d06db311bbb81e8754f7e41e83a559e3
apahie <apahie0918@gmail.com>
2022-08-21 00:29:30 +0900

    fix Rectangle's field name

--- structs-methods-and-interfaces/shape.go
+++ structs-methods-and-interfaces/shape.go
@@ -3,16 +3,16 @@ package main
 import "math"
 
 type Rectangle struct {
-	width  float64
-	height float64
+	Width  float64
+	Height float64
 }
 
 func (r Rectangle) Perimeter() float64 {
-	return 2 * (r.width + r.height)
+	return 2 * (r.Width + r.Height)
 }
 
 func (r Rectangle) Area() float64 {
-	return r.width * r.height
+	return r.Width * r.Height
 }
 
 type Circle struct {
