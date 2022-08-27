0e5996a60b81c4c40f96cf70013ee041006f79e0
apahie <apahie0918@gmail.com>
2022-08-27 23:21:05 +0900

    impl word does not exist pattern

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -3,8 +3,9 @@ package main
 type DictionaryErr string
 
 const (
-	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannnot update word because it does not exist")
 )
 
 func (e DictionaryErr) Error() string {
@@ -35,6 +36,16 @@ func (d Dictionary) Add(word, definition string) error {
 	}
 }
 
-func (d Dictionary) Update(word, definition string) {
-	d[word] = definition
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+		return nil
+	default:
+		return err
+	}
 }
