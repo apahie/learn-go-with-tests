07f78fcea6e2acf34d022f1c3f06103b11e01bce
apahie <apahie0918@gmail.com>
2022-08-28 12:06:30 +0900

    output multiline

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -6,8 +6,16 @@ import (
 	"os"
 )
 
+const (
+	finalWord      = "Go!"
+	countdownStart = 3
+)
+
 func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
 }
 
 func main() {
